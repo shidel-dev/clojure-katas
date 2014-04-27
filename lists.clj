c678d66842f76baabdf0dac2ed2c532c82883236
Shidel <joeshidel@gmail.com>
2014-04-27 11:20:36 -0500

    passes compress test

--- lists.clj
+++ lists.clj
@@ -1,14 +1,19 @@
 (ns lists)
 
 (defn my-last [li]
-  (take-last 1 li)
-)
+  (take-last 1 li))
 
 (defn my-but-last [li]
-  (take-last 2 li)
-)
+  (take-last 2 li))
 
 (defn is-palindrome [li]
-  (= li (reverse li))
-)
+  (= li (reverse li)))
 
+
+(defn compress [li]
+  (loop [compLi '() curLi (reverse li)]
+    (if (empty? curLi) compLi
+      (recur (conj compLi (first curLi)) (drop-while #(= % (first curLi)) curLi)))))
+
+
+(compress '(a a a a b c c a a d e e e e))
