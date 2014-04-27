eab8c4e45d5d97bdb96a57c989939af5dc736a6f
Shidel <joeshidel@gmail.com>
2014-04-27 18:17:29 -0500

    adds test from exercism

--- tests.clj
+++ tests.clj
@@ -46,6 +46,48 @@
 (deftest totient-phi
   (is (= (math/totient 10) 4)))
 
+(deftest prime-factors
+  (is (= (math/prime-factors 315) '(3 3 5 7))))
+
+
+; from exercism.io
+
+(load-file "crypto_square.clj")
+
+(deftest normalize-splunk
+  (is (= "splunk" (normalize-plaintext "s#!@$%plunk"))))
+(deftest normalize-with-punctuation
+  (is (= "123go" (normalize-plaintext "1, 2, 3 GO!"))))
+
+(deftest square-2
+  (is (= 2 (square-size "1234"))))
+(deftest square-3
+  (is (= 3 (square-size "123456789"))))
+(deftest square-4
+  (is (= 4 (square-size "123456789abc"))))
+
+(deftest segments
+  (is (= ["neverv", "exthin", "eheart", "withid", "lewoes"]
+         (plaintext-segments "Never vex thine heart with idle woes."))))
+(deftest segments-2
+  (is (= ["zomg", "zomb", "ies"]
+         (plaintext-segments "ZOMG! ZOMBIES!!!"))))
+
+(deftest cipher-1
+  (is (= "tasneyinicdsmiohooelntuillibsuuml"
+         (ciphertext "Time is an illusion. Lunchtime doubly so."))))
+(deftest cipher-2
+  (is (= "wneiaweoreneawssciliprerlneoidktcms"
+         (ciphertext "We all know interspecies romance is weird."))))
+(deftest cipher-3
+  (is (= "msemo aanin dninn dlaet ltshu i"
+         (normalize-ciphertext "Madness, and then illumination."))))
+(deftest cipher-4
+  (is (= "vrela epems etpao oirpo"
+         (normalize-ciphertext "Vampires are people too!"))))
+
+
+
 
 (run-tests)
 
