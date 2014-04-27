73906de5f677a5473f924822dfd6b346b22b11be
Shidel <joeshidel@gmail.com>
2014-04-27 13:17:17 -0500

    adds test for encode

--- tests.clj
+++ tests.clj
@@ -20,5 +20,8 @@
    (is (= (lists/pack '(a a a a b c c a a d e e e e)) '((a a a a) (b) (c c) (a a) (d) (e e e e)))))
 
 
+(deftest encode
+   (is (=  (lists/encode '(a a a a b c c a a d e e e e)) '((4 a) (1 b) (2 c) (2 a) (1 d)(4 e)))))
+
 (run-tests)
 
